d54e6fb4f96c3564d50292077b1692aa46328607
Gabe Khoury <gabrielkhoury3@gmail.com>
2024-03-12 17:03:00 -0400

    class changes

--- models/genre.js
+++ models/genre.js
@@ -3,8 +3,34 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var GenreSchema = new Schema(
-  {}
+  GenreSchema
+.virtual('lifespan')
+.get(function() {
+  var lifespan = "-";
+  if (this.date_of_birth && this.date_of_death) {
+    lifespan = this.date_of_birth.getFullYear + "-" + this.date_of_death.getFullYear
+  } if (!this.date_of_dath) {
+    lifespan = this.date_of_birth.getFullYear + "-"
+  }
+  return lifespan;
+})
+
 );
+
+var AuthorSchema = new Schema(
+  AuthorSchema
+.virtual('lifespan')
+.get(function() {
+  var lifespan = "-";
+  if (this.date_of_birth && this.date_of_death) {
+    lifespan = this.date_of_birth.getFullYear + "-" + this.date_of_death.getFullYear
+  } if (!this.date_of_dath) {
+    lifespan = this.date_of_birth.getFullYear + "-"
+  }
+  return lifespan;
+})
+);
+
 
 
 //Export model
